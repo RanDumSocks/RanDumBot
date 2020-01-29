aad38d5a113b95f8d1aaa5b9ede83be2c4be670c
RanDumSocks <justinchance@randumsocks.com>
2020-01-29 10:46:18 -0800

    Added jsdocs

--- commands/random.js
+++ commands/random.js
@@ -1,4 +1,4 @@
-exports.run = (argc, argv, client, context) => {
+exports.run = (argc, argv, userstate, RanDumBot) => {
   var minNum = 0;
   var maxNum = 100;
   if (argc == 3) {
@@ -6,5 +6,5 @@ exports.run = (argc, argv, client, context) => {
     minNum = parseInt(argv[1]);
     if (minNum >= maxNum) { throw '!random: bad range given' }
   }
-  client.say(process.env.CHANNEL_NAME, (Math.floor(Math.random() * (maxNum - minNum)) + minNum + 1).toString());
+  RanDumBot.client.say(process.env.CHANNEL_NAME, (Math.floor(Math.random() * (maxNum - minNum)) + minNum + 1).toString());
 }
