d414e63e8f9e3904aa6b596511d2c016e050ba83
RanDumSocks <justinchance@randumsocks.com>
2020-01-28 22:17:44 -0800

    eo Stream 1

--- RanDumBot.js
+++ RanDumBot.js
@@ -20,11 +20,11 @@ function startBot() {
   // Define configuration options
   const opts = {
     identity: {
-      username: process.env.BOT_UNAME,
-      password: process.env.BOT_OAUTH,
+      username: process.env.BOT_USERNAME,
+      password: process.env.OAUTH_TOKEN,
     },
     channels: [
-      process.env.CHANNEL_UNAME,
+      process.env.CHANNEL_NAME,
     ]
   };
 
@@ -36,19 +36,20 @@ function startBot() {
   client.connect();
 
   function onMessageHandler(target, context, msg, self) {
+    if (self) {return;}
 
     if (msg[0] == '!') {
-      parseCommand(msg.slice(1, msg.length));
+      parseCommand(msg.slice(1, msg.length), context);
     } else {
-      logMessage(msg, target);
+      logMessage(msg, context.username);
     }
   }
 
   // Called every time the bot connects to Twitch chat
   function onConnectedHandler(addr, port) {
     debugMsg(`Connected to ${addr}:${port}`);
-    debugMsg('Bot ' + col.cyan(process.env.BOT_UNAME) + ' running on channel ' +
-             col.cyan(process.env.CHANNEL_UNAME));
+    debugMsg('Bot ' + col.cyan(process.env.BOT_USERNAME) + ' running on channel ' +
+             col.cyan(process.env.CHANNEL_NAME));
   }
 
   function onJoinHandler(channel, username, self) {
@@ -64,18 +65,54 @@ function startBot() {
   }
 
   function logMessage(msg, user) {
-    console.log(user.slice(1, user.length).blue + ': ' + msg);
+    console.log(user.cyan + ': ' + msg);
     pushMessage(msg, user);
   }
 
-  function parseCommand(command) {
+  function parseCommand(command, context) {
     const argv = command.split(' ');
     const argc = argv.length;
+    var argContext = 0;
 
-    debugMsg(argv[0], 'Command');
+    debugMsg(context.username + ': ' + argv, 'Command')
+
+    client.deletemessage(process.env.CHANNEL_NAME, context.id).catch((err) => {
+      debugMsg(err, 'Error', col.red)
+    });
+
+    try {
+      switch(argv[0]) {
+        case 'randum':
+        case 'random':
+          var minNum = 0;
+          var maxNum = 100;
+          if (argc == 3) {
+            maxNum = parseInt(argv[2]);
+            minNum = parseInt(argv[1]);
+            if (minNum >= maxNum) { throw new Error('!random: bad range given') }
+          }
+          client.say(process.env.CHANNEL_NAME, (Math.floor(Math.random() * (maxNum - minNum)) + minNum + 1).toString());
+          break;
+        case 'help':
+          if (argc == 2) {
+            switch (argv[1]) {
+              case 'random':
+              case 'randum':
+                client.whisper(context.username, '!random x y: Pick a random number between x and y. Default x = 0, y = 100');
+                break;
+            }
+          } else {
+            client.whisper(context.username, '!random');
+          }
+          break;
+      }
+    } catch (err) {
+      debugMsg(err, 'Error', col.red);
+    }
   }
 
   function pushMessage(msg, user) {
     io.emit('drawMessage', { user: user, msg: msg });
   }
+
 }
--- client.js
+++ client.js
@@ -2,25 +2,111 @@ var canvas  = document.getElementById('canvas-chat');
 var c       = canvas.getContext('2d');
 var socket  = io.connect();
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+resizeWindow(innerWidth, innerHeight);
+
+var instances = [];
+
+// Traps
+var trap_windowResize = false;
+var trap_newMessages = [];
 
 // Listeners
 socket.on('drawMessage', function (data) {
-  console.log(data);
+  var receivedMessage = [data.user, data.msg];
+  trap_newMessages.push(receivedMessage);
 });
 
 window.addEventListener('resize', function(event) {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-
-  c.beginPath();
-  c.rect(5, 5, 10, 10);
-  c.stroke();
+  resizeWindow(innerWidth, innerHeight);
 });
 
-// Setup
+function resizeWindow(width, height) {
+  canvas.width = width;
+  canvas.height = height;
 
-c.beginPath();
-c.rect(5, 5, 10, 10);
-c.stroke();
+  windowResize = true;
+}
+
+function globalUpdate() {
+  window.requestAnimationFrame(globalUpdate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (i in instances) {
+    instances[i].update();
+  }
+
+  // Handle traps
+  trap_windowResize = false;
+  trap_newMessages = [];
+}
+
+// Object Classes
+class Object {
+  constructor() {
+    instances.push(this);
+
+    this.x = 0;
+    this.y = 0;
+  }
+
+  draw() {}
+
+  update() {
+    this.draw();
+    if (trap_windowResize) this.globalWindowResize();
+    if (trap_newMessages.length > 0) this.globalMessageReceived(trap_newMessages);
+  }
+
+  globalWindowResize() {}
+
+  globalMessageReceived(msgs) {}
+}
+
+class Text extends Object {
+  constructor() {
+    super();
+
+    this.fontSize = 30;
+    this.font = 'Arial';
+    this.text = "test :)\ntwtwt";
+    this.x = 5;
+    this.y = 5;
+  }
+
+  draw() {
+    super.draw();
+    c.font = this.fontSize.toString() + 'px ' + this.font;
+    c.textBaseline = 'top';
+    c.fillText(this.text, this.x, this.y);
+  }
+
+  update() {
+    super.update();
+    this.fontSize = textSize;
+  }
+}
+
+class ChatBar extends Object {
+  constructor() {
+    super();
+    this.textSize = 30;
+    this.textLines = 2;
+    this.chatHeight = 100;
+
+    this.y = canvas.height - this.chatHeight;
+  }
+
+  update() {
+    super.update();
+  }
+
+  globalWindowResize() {}
+
+  globalMessageReceived(msgs) {
+    console.log(msgs);
+  }
+}
+
+// Setup
+globalUpdate();
+new ChatBar();
