f572f2ff2b670e50b8e1b1560eb1f9155b2fc815
RanDumSocks <justinchance@randumsocks.com>
2020-03-09 09:59:57 -0700

    help command now handles aliases

--- commands/help.js
+++ commands/help.js
@@ -4,26 +4,38 @@ exports.cmdInfo = {
   bot_version: '0.2.0',
   command_arguments: [ 'command' ],
   description: 'Displays a list of avaliable commands. Can be used as ' +
-               '"!help [command]" to view help for a specific command',
+               '"!help <command>" to view help for a specific command',
   aliases: ['h'],
 };
 
 exports.run = (argc, argv, userstate) => {
-  if (argc == 1) {
+  if (argc == 1) { // List all commands
     var commandString = 'Commands: ';
     for (var i = 0; i < this.RanDumBot.commandMap.length; i += 1) {
-      commandString += '!' + this.RanDumBot.commandMap[i][0] + ', ';
+      var cmdName = this.RanDumBot.commandMap[i][0];
+      var cmdOrigName = this.RanDumBot.commandMap[i][1].data.name;
+      if (cmdName == cmdOrigName) commandString += '!' + cmdName + ', ';
     }
-    this.RanDumBot.client.say(process.env.CHANNEL_NAME, commandString);
-  } else if (argc == 2) {
+
+    this.RanDumBot.say(commandString);
+
+  } else if (argc == 2) { // Info about specific command
+
     for (var i = 0; i < this.RanDumBot.commandMap.length; i += 1) {
+
       if (this.RanDumBot.commandMap[i][0] == argv[1]) {
-        var commandName = this.RanDumBot.commandMap[i][0];
         var command = this.RanDumBot.commandMap[i][1];
+        var commandName = command.data.name;
         var commandArguments = command.cmdInfo.command_arguments;
+        var commandAliases = command.cmdInfo.aliases;
         var commandNameFormat = '!' + commandName;
+
+        for (var i = 0; i < commandAliases.length; i++) {
+          commandNameFormat += `, !${commandAliases[i]}`;
+        }
+
         for (var i = 0; i < commandArguments.length; i++) {
-          commandNameFormat += ` [${commandArguments[i]}]`
+          commandNameFormat += ` <${commandArguments[i]}>`
         }
 
         this.RanDumBot.client.say(process.env.CHANNEL_NAME,
@@ -31,5 +43,6 @@ exports.run = (argc, argv, userstate) => {
         break;
       }
     }
+
   }
 }
